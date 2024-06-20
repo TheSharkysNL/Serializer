4430d9197915d49c7a052f9cde1eac5e313da8b7
TheSharkysNL <mcyuillian@me.com>
2024-06-20 20:28:27 +0200

    Added the ISerializable interface

---
+++ Serializer/ISerializable.cs
@@ -0,0 +1,17 @@
+using Microsoft.Win32.SafeHandles;
+
+namespace Serializer;
+
+public interface ISerializable<T>
+    where T : ISerializable<T>
+{
+    public static abstract T Deserialize(string filename);
+    public static abstract T Deserialize(string filename, long offset);
+    public static abstract T Deserialize(SafeFileHandle handle);
+    public static abstract T Deserialize(SafeFileHandle handle, long offset);
+
+    public long Serialize(string filename);
+    public long Serialize(string filename, long offset);
+    public long Serialize(SafeFileHandle filename);
+    public long Serialize(SafeFileHandle filename, long offset);
+}

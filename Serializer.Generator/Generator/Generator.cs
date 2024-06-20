161851c1cec2294a1e08add35b25657a2a1b6e9d
TheSharkysNL <mcyuillian@me.com>
2024-06-20 23:27:08 +0200

    got serializable members from type

--- Serializer.Generator/Generator/Generator.cs
+++ Serializer.Generator/Generator/Generator.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.Immutable;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Serializer.Extensions;
 
@@ -29,6 +31,15 @@ public class Generator : ISourceGenerator
             }
             
             SemanticModel model = compilation.GetSemanticModel(inheritingType.SyntaxTree);
+
+            INamedTypeSymbol? symbol = model.GetDeclaredSymbol(inheritingType, token);
+            if (symbol is null)
+            {
+                continue;
+            }
+            
+            ImmutableArray<ISymbol> members = symbol.GetMembers();
+            IEnumerable<(string name, ITypeSymbol type)> serializableMembers = GetSerializableMembers(members);
         }
         
         context.AddSource("test.g.cs", generatedCode.ToString());
@@ -37,5 +48,55 @@ public class Generator : ISourceGenerator
     public void Initialize(GeneratorInitializationContext context)
     {
         context.RegisterForSyntaxNotifications(() => new InheritingTypesSyntaxReceiver());
+    }
+
+    private IEnumerable<(string name, ITypeSymbol type)> GetSerializableMembers(ImmutableArray<ISymbol> members)
+    {
+        for (int i = 0; i < members.Length; i++)
+        {
+            ISymbol symbol = members[i];
+
+            if (symbol is IFieldSymbol field &&
+                IsSerializableField(field))
+            {
+                yield return (field.Name, field.Type);
+            }
+
+            if (symbol is IPropertySymbol property && 
+                IsSerializableProperty(property)){
+                yield return (property.Name, property.Type);
+            }
+        }
+    }
+
+    private bool IsSerializableField(IFieldSymbol field) =>
+        !HasBackingFieldCharacters(field) && field is { IsConst: false, IsStatic: false };
+
+    private bool HasBackingFieldCharacters(IFieldSymbol field) =>
+        field.Name.AsSpan().Contains("<".AsSpan(), StringComparison.Ordinal);
+    
+    private bool IsSerializableProperty(IPropertySymbol property) =>
+        HasDefaultGetterName(property) && !property.IsStatic;
+
+    private bool HasDefaultGetterName(IPropertySymbol property)
+    {
+        if (property.GetMethod is null)
+        {
+            return false;
+        }
+        
+        const string getMethodName = "get_";
+        string propertyName = property.Name;
+
+        /* property name should never be more than 512 characters long
+         * https://www.codeproject.com/Questions/502735/What-27splustheplusmaxpluslengthplusofplusaplusplu
+         * so stackalloc is safe as the maximum characters is 515 which is 1030 bytes
+         */
+        Span<char> tempNameSpan = stackalloc char[getMethodName.Length + propertyName.Length]; 
+        
+        getMethodName.AsSpan().CopyTo(tempNameSpan);
+        propertyName.AsSpan().CopyTo(tempNameSpan.Slice(getMethodName.Length));
+
+        return property.GetMethod.Name.AsSpan().SequenceEqual(tempNameSpan);
     }
 }

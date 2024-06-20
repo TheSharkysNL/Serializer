14353e72ab14fed792274c4e6209be7bc9a809ae
TheSharkysNL <mcyuillian@me.com>
2024-06-20 22:27:59 +0200

    added TypeDeclarationSyntaxExtensions and InheritsFrom function

--- Serializer.Generator/Generator/InheritingTypesSyntaxReceiver.cs
+++ Serializer.Generator/Generator/InheritingTypesSyntaxReceiver.cs
@@ -12,8 +12,6 @@ public class InheritingTypesSyntaxReceiver : ISyntaxReceiver
 
     protected List<TypeDeclarationSyntax> inhertingTypes = new(16);
     
-    private static readonly char[] typeSeparators = [ '.', ':' ]; // global::namespace.class
-    
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
         if (syntaxNode is not TypeDeclarationSyntax type || 
@@ -24,35 +22,5 @@ public class InheritingTypesSyntaxReceiver : ISyntaxReceiver
         }
         
         inhertingTypes.Add(type);
-
-        // if (syntaxNode is not SimpleBaseTypeSyntax { Type: SimpleNameSyntax nameSyntax } || 
-        //     !HasName(nameSyntax, SerializableInterfaceName))
-        // {
-        //     return;
-        // }
-
-        // //                      BaseList.TypeDeclaration
-        // Debug.Assert(syntaxNode.Parent!.Parent is TypeDeclarationSyntax);
-        // TypeDeclarationSyntax type = (TypeDeclarationSyntax)syntaxNode.Parent.Parent!;
-
-        // SerializableCandidates.Add(type);
     }
-
-    protected bool HasName(SimpleNameSyntax syntax, string name) =>
-        GetName(syntax).SequenceEqual(name.AsSpan());
-
-    protected ReadOnlySpan<char> GetName(SimpleNameSyntax syntax) =>
-        GetName(syntax.Identifier.Text);
-
-    protected ReadOnlySpan<char> GetName(string identifier)
-    {
-        int startIndex = identifier.LastIndexOfAny(typeSeparators) + 1;
-        
-        int genericIndex = identifier.IndexOf('<', startIndex); // generics
-        int endIndex = genericIndex == -1 ? identifier.Length : genericIndex;
-
-        int length = endIndex - startIndex;
-
-        return identifier.AsSpan(startIndex, length);
-    } 
 }

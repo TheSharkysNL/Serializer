4ebb1ff99b474cd6be2e81569e06c740524192af
TheSharkysNL <mcyuillian@me.com>
2024-06-20 21:35:41 +0200

    added test class

--- Serializer.Test/Program.cs
+++ Serializer.Test/Program.cs
@@ -1,3 +1,45 @@
-﻿using Serializer;
+﻿using Microsoft.Win32.SafeHandles;
+using Serializer;
 
-Console.WriteLine("Hello, World!");
+public partial class Test : ISerializable<Test>
+{
+    public static Test Deserialize(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Test Deserialize(string filename, long offset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Test Deserialize(SafeFileHandle handle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Test Deserialize(SafeFileHandle handle, long offset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Serialize(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Serialize(string filename, long offset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Serialize(SafeFileHandle filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Serialize(SafeFileHandle filename, long offset)
+    {
+        throw new NotImplementedException();
+    }
+}

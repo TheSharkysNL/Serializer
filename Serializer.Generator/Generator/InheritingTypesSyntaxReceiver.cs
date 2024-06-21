2817dded4bcca663ce4921d311fa77abbefdf499
TheSharkysNL <mcyuillian@me.com>
2024-06-21 11:44:40 +0200

    added super type candidates

--- Serializer.Generator/Generator/InheritingTypesSyntaxReceiver.cs
+++ Serializer.Generator/Generator/InheritingTypesSyntaxReceiver.cs
@@ -3,23 +3,37 @@ using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Serializer.Extensions;
 
 namespace Serializer.Generator;
 
-public class InheritingTypesSyntaxReceiver : ISyntaxReceiver
+public class InheritingTypesSyntaxReceiver(string name) : ISyntaxReceiver
 {
     public virtual IEnumerable<TypeDeclarationSyntax> Candidates => inhertingTypes;
+    public virtual IEnumerable<TypeDeclarationSyntax> SuperTypeCandidates => superTypes;
 
     protected List<TypeDeclarationSyntax> inhertingTypes = new(16);
+    protected List<TypeDeclarationSyntax> superTypes = new(8);
     
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
-        if (syntaxNode is not TypeDeclarationSyntax type || 
-            type.Modifiers.IndexOf(SyntaxKind.PartialKeyword) == -1 || // check for partial keyword
+        if (syntaxNode is not TypeDeclarationSyntax type)
+        {
+            return;
+        }
+
+        if (type.HasName(name))
+        {
+            superTypes.Add(type);
+            return;
+        }
+        
+        if (type.Modifiers.IndexOf(SyntaxKind.PartialKeyword) == -1 || // check for partial keyword
             type.BaseList is null) 
         {
             return;
         }
+        
         
         inhertingTypes.Add(type);
     }

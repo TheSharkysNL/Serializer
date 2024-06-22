a324516a3764ec97e3f78c568c77cc2ec36e69a8
TheSharkysNL <mcyuillian@me.com>
2024-06-22 13:43:19 +0200

    namespace type and methods generate, still a wip

--- Serializer.Generator/Generator/Generator.cs
+++ Serializer.Generator/Generator/Generator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.Buffers;
+using System.Collections.Immutable;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
@@ -15,7 +16,7 @@ public class Generator : ISourceGenerator
     private const string SerializableName = "ISerializable";
     private const string SerializableFullNamespace = $"global::Serializer.{SerializableName}";
 
-    private static readonly string[][] SerializableFunctions = [
+    private static readonly string[][] serializableFunctions = [
         ["Deserialize", "static", "global::System.String"], 
         ["Deserialize", "static", "global::System.String", "global::System.Int64"],
         ["Deserialize", "static", "global::Microsoft.Win32.SafeHandles.SafeFileHandle"],
@@ -35,7 +36,7 @@ public class Generator : ISourceGenerator
 
         IEnumerable<TypeDeclarationSyntax> inheritingTypes = receiver.Candidates;
         
-        StringBuilder generatedCode = new StringBuilder(4096);
+        StringBuilder generatedCode = new(4096);
         foreach (TypeDeclarationSyntax inheritingType in inheritingTypes)
         {
             InheritingTypes type = inheritingType.InheritsFrom(SerializableFullNamespace, compilation, token);
@@ -51,8 +52,52 @@ public class Generator : ISourceGenerator
             {
                 continue;
             }
+
+            string fullTypeName = symbol.ToDisplayString(Formats.GlobalFullNamespaceFormat);
+
+            generatedCode.Append("namespace ");
+            generatedCode.Append(GetNamespaceFromFullTypeName(fullTypeName));
+            generatedCode.Append(" { ");
+
+            AddModifiers(generatedCode, inheritingType.Modifiers);
+            generatedCode.Append(inheritingType.IsKind(SyntaxKind.ClassDeclaration) ? " class " : " struct ");
+            generatedCode.Append(symbol.Name);
+            generatedCode.Append(" { ");
             
             ImmutableArray<ISymbol> members = symbol.GetMembers();
+            foreach ((string name, string modifiers, ReadOnlyMemory<string> parameters) in GetNonImplementMethods(members))
+            {
+                generatedCode.Append("public ");
+                generatedCode.Append(modifiers);
+                generatedCode.Append(' ');
+                generatedCode.Append(modifiers.Contains("static")
+                    ? fullTypeName
+                    : "global::System.Int64");
+
+                generatedCode.Append(' ');
+                generatedCode.Append(name);
+
+                generatedCode.Append('(');
+                if (parameters.Length != 0) // temporary
+                {
+                    generatedCode.Append(parameters.Span[0]);
+                    generatedCode.Append(' ');
+                    generatedCode.Append((char)(0 + 'a'));
+                    for (int i = 1; i < parameters.Length; i++)
+                    {
+                        generatedCode.Append(',');
+                        
+                        string paramType = parameters.Span[i];
+                        generatedCode.Append(paramType);
+                        generatedCode.Append(' ');
+                        generatedCode.Append((char)(i + 'a'));
+                    }
+                }
+
+                generatedCode.Append(')');
+                generatedCode.Append("{ throw new global::System.NotImplementedException(); }");
+            }
+            
             IEnumerable<(string name, ITypeSymbol type)> serializableMembers = GetSerializableMembers(members);
 
             foreach ((string memberName, ITypeSymbol memberType) in serializableMembers)
@@ -60,14 +105,115 @@ public class Generator : ISourceGenerator
                 
             }
             
+            generatedCode.Append(" } }");
         }
         
-        // context.AddSource("test.g.cs", generatedCode.ToString());
+        context.AddSource("test.g.cs", generatedCode.ToString());
     }
 
     public void Initialize(GeneratorInitializationContext context)
     {
         context.RegisterForSyntaxNotifications(() => new InheritingTypesSyntaxReceiver());
+    }
+
+    private ReadOnlySpan<char> GetNamespaceFromFullTypeName(string fullTypeName)
+    {
+        int globalIndex = fullTypeName.IndexOf(':');
+        int startIndex = globalIndex == -1 ? 0 : globalIndex + 2;
+
+        int endIndex = fullTypeName.LastIndexOf('.');
+        if (endIndex == -1)
+        {
+            throw new ArgumentException($"type: {fullTypeName} is not contained within a namespace");
+        }
+
+        int length = endIndex - startIndex;
+        return fullTypeName.AsSpan(startIndex, length);
+    }
+
+    private void AddModifiers(StringBuilder builder, SyntaxTokenList modifiers)
+    {
+        if (modifiers.Count == 0)
+        {
+            return;
+        }
+
+        builder.Append(modifiers[0].Text);
+        for (int i = 1; i < modifiers.Count; i++)
+        {
+            builder.Append(' ');
+            builder.Append(modifiers[i].Text);
+        }
+    }
+
+    private IEnumerable<(string name, string modifiers, ReadOnlyMemory<string> parameters)> GetNonImplementMethods(ImmutableArray<ISymbol> members)
+    {
+        for (int i = 0; i < serializableFunctions.Length; i++)
+        {
+            string[] function = serializableFunctions[i];
+            
+            string funcName = function[0];
+            string modifiers = function[1];
+            ReadOnlyMemory<string> parameters = ReadOnlyMemory<string>.Empty;
+            if (function.Length > 2)
+            {
+                parameters = function.AsMemory()[2..];
+            }
+
+            IMethodSymbol? method = FindMethod(members, funcName, parameters.Span);
+            if (method is null)
+            {
+                yield return (funcName, modifiers, parameters);
+            }
+            else if (method.IsPartialDefinition)
+            {
+                yield return (funcName, modifiers + " partial", parameters);
+            }
+        }
+    }
+
+    private IMethodSymbol? FindMethod(ImmutableArray<ISymbol> symbols, string name, ReadOnlySpan<string> parameterTypes)
+    {
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            ISymbol symbol = symbols[i];
+            if (symbol.Kind != SymbolKind.Method)
+            {
+                continue;
+            }
+
+            IMethodSymbol method = (IMethodSymbol)symbol;
+            if (method.Name == name &&
+                HasParameterTypes(method, parameterTypes))
+            {
+                return method;
+            }
+        }
+
+        return null;
+    }
+
+    private bool HasParameterTypes(IMethodSymbol method, ReadOnlySpan<string> types)
+    {
+        ImmutableArray<IParameterSymbol> parameters = method.Parameters;
+        if (parameters.Length != types.Length)
+        {
+            return false;
+        }
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            IParameterSymbol parameter = parameters[i];
+            string type = types[i];
+            ReadOnlySpan<char> shortName = type.GetShortName();
+
+            if (!parameter.Type.Name.AsSpan().SequenceEqual(shortName) ||
+                !parameter.Type.FullNamesMatch(type))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private TypeDeclarationSyntax? GetSuperType(IEnumerable<TypeDeclarationSyntax> superTypes, Compilation compilation)
@@ -82,7 +228,7 @@ public class Generator : ISourceGenerator
                 continue;
             }
 
-            string name = symbol.ToDisplayString(Formats.FullNamespaceFormat);
+            string name = symbol.ToDisplayString(Formats.GlobalFullNamespaceFormat);
             if (name == SerializableFullNamespace)
             {
                 return superType;

525ced1481cfb13406dc1c7d4c947859f2cbb90f
TheSharkysNL <mcyuillian@me.com>
2024-06-25 16:44:30 +0200

    added stream serialization

--- Serializer/ISerializable.cs
+++ Serializer/ISerializable.cs
@@ -9,9 +9,11 @@ public interface ISerializable<T>
     public static abstract T Deserialize(string filename, long offset);
     public static abstract T Deserialize(SafeFileHandle handle);
     public static abstract T Deserialize(SafeFileHandle handle, long offset);
+    public static abstract T Deserialize(Stream stream);
 
     public long Serialize(string filename);
     public long Serialize(string filename, long offset);
     public long Serialize(SafeFileHandle filename);
     public long Serialize(SafeFileHandle filename, long offset);
+    public long Serialize(Stream stream);
 }
